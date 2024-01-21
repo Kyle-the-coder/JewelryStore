1f8924f07e0b8ed2bb47a9c89c33217e3b9f183b
Kyle-the-coder <kylethacoder@gmail.com>
2024-01-20 16:43:26 -0800

    updated

--- src/components/pageDesign/MapComp.jsx
+++ src/components/pageDesign/MapComp.jsx
@@ -2,10 +2,8 @@ import React, { useEffect } from "react";
 
 const MapComponent = () => {
   useEffect(() => {
-    // Replace 'YOUR_API_KEY' with your actual Google Maps API key
     const apiKey = import.meta.env.VITE_API_KEY;
 
-    // Check if the Google Maps API script is already loaded
     if (!window.google) {
       const script = document.createElement("script");
       script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
@@ -14,28 +12,21 @@ const MapComponent = () => {
       document.head.appendChild(script);
 
       script.onload = () => {
-        // Initialize the map after the script is loaded
         initMap();
       };
     } else {
-      // Initialize the map if the script is already loaded
       initMap();
     }
 
     function initMap() {
-      // Create a geocoder object
       const geocoder = new window.google.maps.Geocoder();
 
-      // Specify the address you want to geocode
       const address = "647 4th St, Santa Rosa, CA 95404";
 
-      // Make a geocoding request
       geocoder.geocode({ address: address }, (results, status) => {
         if (status === "OK") {
-          // Get the latitude and longitude
           const location = results[0].geometry.location;
 
-          // Create a map centered at the specified location
           const map = new window.google.maps.Map(
             document.getElementById("map"),
             {
@@ -44,7 +35,6 @@ const MapComponent = () => {
             }
           );
 
-          // Add a marker for the specified location
           new window.google.maps.Marker({
             map: map,
             position: location,
