5a97a220b8790ab17a98f409e9e2e949ab179569
Kyle-the-coder <kylethacoder@gmail.com>
2024-01-21 09:50:55 -0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Change this to your desired output directory
+    outDir: "dist",
   },
   define: {
     "process.env": dotenv.config().parsed,
